08a0ddcd11de2f74a7b1fd137a6493fea9e1e5bc
Jaehyun <1992jhlee@gmail.com>
2017-07-28 18:16:16 +0900

    change hyperparameter

--- train.py
+++ train.py
@@ -32,29 +32,29 @@ X = tf.placeholder(tf.float32, [None, 15, 15, 3])
 Y = tf.placeholder(tf.float32, [None, 225])
 # Y = tf.placeholder(tf.float32, [1, 225])
 
-W1 = tf.Variable(tf.random_normal([7, 7, 3, 128], stddev=0.1), name="W1")
+W1 = tf.Variable(tf.random_normal([7, 7, 3, 256], stddev=0.1), name="W1")
 L1 = tf.nn.conv2d(X, W1, strides=[1, 1, 1, 1], padding='SAME')
 L1 = tf.nn.relu(L1) # output shape = (1, 15, 15, 128)
 
-W2 = tf.Variable(tf.random_normal([5, 5, 128, 128], stddev=0.1), name="W2")
+W2 = tf.Variable(tf.random_normal([5, 5, 256, 256], stddev=0.1), name="W2")
 L2 = tf.nn.conv2d(L1, W2, strides=[1, 1, 1, 1], padding='SAME')
 L2 = tf.nn.relu(L2) # output shape = (1, 15, 15, 128)
 
-W3 = tf.Variable(tf.random_normal([3, 3, 128, 128], stddev=0.1), name="W3")
+W3 = tf.Variable(tf.random_normal([3, 3, 256, 256], stddev=0.1), name="W3")
 L3 = tf.nn.conv2d(L2, W3, strides=[1, 1, 1, 1], padding='SAME')
 L3 = tf.nn.relu(L3) # output shape = (1, 15, 15, 128)
 
-W4 = tf.Variable(tf.random_normal([3, 3, 128, 128], stddev=0.1), name="W4")
+W4 = tf.Variable(tf.random_normal([3, 3, 256, 256], stddev=0.1), name="W4")
 L4 = tf.nn.conv2d(L3, W4, strides=[1, 1, 1, 1], padding='SAME')
 L4 = tf.nn.relu(L4) # output shape = (1, 15, 15, 128)
-L4_flat = tf.reshape(L4, [-1, 15*15*128])
+L4_flat = tf.reshape(L4, [-1, 15*15*256])
 #W5 = tf.Variable(tf.random_normal([3, 3, 128, 128], stddev=0.1), name="W5")
 #L5 = tf.nn.conv2d(L4, W5, strides=[1, 1, 1, 1], padding='SAME')
 #L5 = tf.nn.relu(L5) # output shape = (1, 15, 15, 128)
 #L5_flat = tf.reshape(L5, [-1, 15*15*128])
 
 # FCNN
-W5 = tf.get_variable("W5", shape=[15*15*128, 225],
+W5 = tf.get_variable("W5", shape=[15*15*256, 225],
                     initializer=tf.contrib.layers.xavier_initializer())
 b = tf.Variable(tf.random_normal([225]))
 logits = tf.matmul(L4_flat, W5) + b
@@ -106,11 +106,11 @@ with tf.Session() as sess:
         print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.9f}'.format(avg_cost), file=f)
         print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.9f}'.format(avg_cost))
         if epoch % 10 == 0:
-            save_path = saver.save(sess, str(os.getcwd()) + "/trained_model/trained_model_at_epoch" + str(epoch) + ".ckpt")
+            save_path = saver.save(sess, str(os.getcwd()) + "//trained_model//trained_model_at_epoch" + str(epoch) + ".ckpt")
             print("Model saved in file: %s" % save_path)
 
     # save completely trained model
-    save_path = saver.save(sess, str(os.getcwd()) + "/trained_model/trained_model.ckpt")
+    save_path = saver.save(sess, str(os.getcwd()) + "//trained_model//trained_model.ckpt")
     print("Model saved in file: %s" % save_path)
 
     f.close()
